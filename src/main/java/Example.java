22cebc0aabf1ff3f3ffb9e7266ec2288dab67bb0
Cletus Lee <clet.lee@gmail.com>
2018-04-06 19:37:12 +0800

    changed message

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -9,7 +9,7 @@ public class Example {
 
     @RequestMapping("/")
     String home() {
-        return "Hello World - v3! Cletus";
+        return "Hello World - v3! Cletus2";
     }
 
     public static void main(String[] args) throws Exception {
