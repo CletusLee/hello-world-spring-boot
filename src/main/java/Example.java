13f878c583a0d3e35a42d38bfb3eed5a7b643057
Cletus Lee <clet.lee@gmail.com>
2018-04-11 19:57:05 +0800

    Pull images from ECR.

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -9,7 +9,7 @@ public class Example {
 
     @RequestMapping("/")
     String home() {
-        return "Hello World!";
+        return "Hello World! Image from ECR!!";
     }
 
     @RequestMapping("/test")
